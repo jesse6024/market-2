68ebb478e6847792be90013dd0eb2d33d9660e38
jesse6024 <60897213+jesse6024@users.noreply.github.com>
2022-12-20 13:09:57 -0500

    Latest Working Build
    Latest Working Build

--- dashboard.php
+++ dashboard.php
@@ -1,35 +1,35 @@
-<?php
-require('db.php');
-include("auth.php");
-?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
 <title>Dashboard - Secured Page</title>
-<link rel="stylesheet" href="style2.css" />
+<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="password-strength-indicator.css">
 </head>
 <body>
-<div class="login-banner" style=""
->
-<div class="login-head-container" style="">
-<div class="header-title" style="
-    text-align: center;
-    "
-><h1>MARKET</h1></div></div>
+<div class="container" style="">
+        <h1>REUP MARKET</h1>
+        <h5>Dashboard</h5>
+
+    </div>
+    <div class="container" style="">
+     
+
+     <header></header>
+     <form style="">
 <div class="form" style= " 
-width: 300px;
+    border:none;
+    width: 300px;
     margin: 0 auto;
-    border: solid 1px black;
+    /*border: solid 1px black;*/
     text-align: center;
     margin-top: 15%;
     overflow: hidden;
 ">
 <h2>DASHBOARD</h2> 
-<div class="form">
+<div class="form" style="border:none;">
 <p>Dashboard</p>
 <p>This is another secured page.</p>
-<p><a href="index2.php">Home</a></p>
 <a href="logout.php">Logout</a>
 </div>
 </body>
